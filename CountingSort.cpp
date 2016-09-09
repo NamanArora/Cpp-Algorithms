98941aea8b1a11ed5b9e6a887301fff62fccc55f
Naman Arora <namanarora11@gmail.com>
2016-09-10 00:33:15 +0530

    Create CountingSort.cpp

---
+++ CountingSort.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <algorithm> 
+#include <bits/stdc++.h> 
+#define N 9 
+using namespace std;
+
+void counting(int a[])
+{
+    int count[9]={0};
+    int out[9]={0};
+    for(int i=0; i<9; i++)
+        ++count[a[i]];
+    
+    for(int i=1; i<9; i++)
+        count[i]+=count[i-1];
+        
+    for(int i=0; i<9; i++)
+    {
+        //because the first number will have count of 1 and we need to 
+        //make it 0 in order to correctly fit inside sorted array.
+        out[count[a[i]]-1]=a[i];
+        --count[a[i]];
+    }
+    for(int i=0; i<9; i++)
+        cout << out[i] << " ";
+}
+
+int main()
+{
+    int a[]={1,4,2,5,8,5,2,2,6};
+    counting(a);
+}
